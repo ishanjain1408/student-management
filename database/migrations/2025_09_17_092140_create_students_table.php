0ad489358d51744548d8429df9c799abe18b85ef
Ishan Jain <ishanjain1408@gmail.com>
2025-09-17 17:51:28 +0530

    added migration

--- database/migrations/2025_09_17_092140_create_students_table.php
+++ database/migrations/2025_09_17_092140_create_students_table.php
@@ -4,24 +4,37 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateStudentsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->date('dob');
+            $table->string('contact');
+            $table->string('gender')->nullable();
+            $table->string('class');
+            $table->string('section')->nullable();
+            $table->string('previous_school')->nullable();
+            $table->text('disabilities')->nullable();
+            $table->longText('photo')->nullable(); // To store the Base64 image data
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('students');
     }
-};
+}
